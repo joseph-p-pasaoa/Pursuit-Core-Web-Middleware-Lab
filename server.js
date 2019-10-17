e7ea0f4cf81c58b11ef69cf6f60b2532eddd2a0c
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2019-10-17 14:52:22 -0400

    0.3, backend/server complete

--- server.js
+++ server.js
@@ -18,9 +18,10 @@ const path = require('path');
 app.listen(port, () => {
     log(`JoeyServer is now the talkative fly on the localhost wall at port ${port}. Cogito ergo sum.`);
 });
-// app.use("/", express.static(path.join(__dirname, "errors")));
 
 
+/* SERVER MAIN */
+// Animal Route //
 const zooRoster = {
   zebra: 12,
   lion: 4,
@@ -40,8 +41,71 @@ const isAnimal = (req, res, next) => {
 app.get("/animal/:query", isAnimal);
 
 
+// Random Route //
+const generateSpread = (req, res, next) => {
+  res.locals.output = {
+    status: "success",
+    range: [parseInt(req.query.floor), parseInt(req.query.ceil)],
+  }
+  if (res.locals.output.range[0] >= res.locals.output.range[1]) {
+    res.status(400).json({
+      status: "failure",
+      message: "error: your must be at least 2 larger than your floor. please check your inputs and try again"
+    })
+  } else {
+    res.locals.array = [];
+    for (let i = res.locals.output.range[0] + 1; i < res.locals.output.range[1]; i ++) {
+      res.locals.array.push(i);
+    }
+    next();
+  }
+};
+const selectNum = (req, res, next) => {
+  res.locals.output['randPick'] = res.locals.array[Math.floor(Math.random() * res.locals.array.length)];
+  res.json(res.locals.output);
+};
+app.get("/random", generateSpread, selectNum);
+
+
+// Queue Route //
+let volunteers = ['alice', 'tracy', 'xavier', 'dustin', 'michelle', 'corey', 'reed'];
+const handleQueue = (req, res, next) => {
+  switch (req.params.op) {
+    case 'enqueue':
+      volunteers.unshift(req.query.name);
+      res.json({
+          status: "success",
+          enqueued: req.query.name
+      });
+      break;
+    case 'dequeue':
+      const dequeued = volunteers[volunteers.length - 1];
+      volunteers.pop();
+      res.json({
+          status: "success",
+          dequeued: dequeued
+      });
+      break;
+    case 'peek':
+      res.json({
+          status: "success",
+          data: volunteers[volunteers.length - 1]
+      });
+      break;
+    default:
+      res.status(404).json({
+          status: "failure",
+          message: "error: path not found"
+      });
+  }
+}
+app.get("/queue/:op", handleQueue);
+
+
+/* NO ROUTE CATCH */
 app.use("*", (req, res) => {
-    res.status(200).json({
-        status: "failure"
+    res.status(404).json({
+        status: "failure",
+        message: "error: path not found"
     });
 });
